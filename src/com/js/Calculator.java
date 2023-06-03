0d7c80f2ef094535cc139db3f1e5945f81d89496
Lakshmip13 <lakshmi97kal@gmail.com>
2023-06-03 11:39:10 +0530

    message

---
+++ src/com/js/Calculator.java
@@ -0,0 +1,19 @@
+package com.js;
+
+public class Calculator {
+
+	public static void main(String[] args) {
+		int a=10;
+		int b=20;
+		int res=a+b;
+		System.out.println("Addition: "+res);
+		int res1=a-b;
+		System.out.println("Substraction: "+res1);
+		int res2=a*b;
+		System.out.println("Multiplicstion: "+res2);
+		int res3=b/a;
+		System.out.println("Division: "+res3);
+
+	}
+
+}
